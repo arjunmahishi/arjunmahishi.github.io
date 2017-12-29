2d8fc6c93ad01546dd4000f7f47c178773107dcb
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-12-29 20:31:59 +0530

    Designed project card

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
-//// Custom Router ////
+//// Router ////
 const pages = [
 	'main',
 	'publications',
 	'projects'
 ];
 
-window.addEventListener("hashchange", () => {
+const openPage = (target) => {
 	pages.map(page => {
-		if(page === location.hash.replace("#", "")){
+		if(page === target){
 			document.querySelector(`#${page}`).style.display = "block";
 		}else{
 			document.querySelector(`#${page}`).style.display = "none";
 		}
 	})
+}
+
+window.addEventListener("hashchange", () => {
+	openPage(location.hash.replace("#", ""));
 });
 
+if(location.hash === "") location.hash = "#main"; // Edge case
+openPage(location.hash.replace("#", "")); // First load
+
 //// Color changer ////
+
+const colorFrequency = 250; // milli seconds
 
 const randomHexColorCode = () => {
 	let n = ((Math.random() * 0xfffff) | 0).toString(16);
@@ -23,11 +32,28 @@ const randomHexColorCode = () => {
 };
 
 setInterval(() => {
+	
 	document.querySelector(".name").style.color = randomHexColorCode();
-	Array.prototype.slice.call(document.querySelectorAll(".btn")).map(button => {
+
+	Array.prototype.slice.call(document.querySelectorAll(".nav-btn")).map(button => {
 		button.style.borderColor = randomHexColorCode();
 		button.style.color = randomHexColorCode();
 	})
-}, 250);
 
-////  ////
+}, colorFrequency);
+
+//// Projects updater ////
+
+fetch("projects.json").then((data) => {
+	console.log(data.body);
+})
+
+// `<div class="card">
+//     <div class="card-title">Test title</div>
+//     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+//     <a href="#">
+//         <button class="btn">Link</button>
+//     </a>
+// </div>`
+
+//// Publications updater ////
