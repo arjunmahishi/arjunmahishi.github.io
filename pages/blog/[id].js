905c98287b2260a107a0f682c9cdfdbc9f24a094
Arjun Mahishi <arjun.mahishi@gmail.com>
2023-03-19 18:50:21 +0530

    add basic dynamic routing with markdown parser

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,26 +1,39 @@
-import fs from 'fs';
-
-const getAllPostIds = () => {
-  const fileNames = fs.readdirSync(postsDirectory);
-
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
-    }
-  })
-}
+import { getPostData, getAllPostIds } from "../../lib/posts";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
+
   return {
     paths,
     fallback: false,
   };
 }
 
-export default function Post() {
-  console.log(getAllPostIds())
-  return <div>...</div>;
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  console.log("debug", postData)
+
+  return (
+    <div>
+      <h1>{postData.title}</h1>
+      <i>{postData.date}</i><br />
+      <b>tags</b>
+      <ul>
+        {postData.tags.map((tag, i) => (
+          <li key={i}>{tag}</li>
+        ))}
+      </ul>
+
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 export default function Home() {
   return (
