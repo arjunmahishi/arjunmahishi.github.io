854d4d3c7820001431b63ba4aadab081475a69a7
Arjun Mahishi <arjun.mahishi@gmail.com>
2023-05-24 19:37:41 +0530

    add google analytics

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,26 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+
+import * as gtag from "../lib/gtag"
 
 export default function Layout({ children, meta }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   if (meta === undefined) meta = {}
 
   return (
@@ -19,6 +38,22 @@ export default function Layout({ children, meta }) {
 
         <link href="/gruvbox.css" rel="stylesheet" />
       </Head>
+
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-857DV9833V" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-857DV9833V', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <div className="grid place-items-center">{children}</div>
 
