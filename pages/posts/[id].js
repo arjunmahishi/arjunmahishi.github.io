07a874626b13b70c7d8e7d0561e25130f6fe3209
Arjun Mahishi <arjun.mahishi@gmail.com>
2023-09-28 15:54:38 +0530

    Blog layout improvements
    * autogenerate anchor tags
    * autogenerate table of contents if enabled
    * Accept hero image as part of greymatter metadata
    * Insert hero Image if present

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -22,9 +22,17 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ postData }) {
+  const showImage = postData.image && postData.image.length > 0;
+
   return (
     <Layout meta={postData}>
       <Nav />
+
+      <h1 className="text-4xl font-bold text-center my-8">{postData.title}</h1>
+
+      <img
+          src={postData.image} alt={postData.title} className="lg:max-w-3xl rounded-xl"
+          style={{ display: showImage ? "block" : "none" }} />
 
       <article
           className="prose lg:max-w-2xl lg:prose-lg px-8 m-auto my-4 sm:my-16 prose-img:rounded-xl"
